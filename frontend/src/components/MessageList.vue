ded2e8efc2ddf0596eb85564c9636f6d061fcf54
Derzsi Dezső <derzsidezso@gmail.com>
2025-03-19 14:02:59 +0200

    BotStatus, MessageList components added and integrated in the app.vue

---
+++ frontend/src/components/MessageList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="message-list">
+    <h2>🔔 Recent Notifications</h2>
+    <ul v-if="messages.length > 0">
+      <li v-for="(msg, index) in messages" :key="index">
+        <strong>[{{ msg.timestamp }}] {{ msg.username }}</strong> in #{{ msg.channel }}:
+        {{ msg.content }}
+      </li>
+    </ul>
+    <p v-else>No notifications yet...</p>
+  </div>
+</template>
+
+<script setup>
+import { useSocketStore } from '@/stores/useSocketStore'
+
+const { messages } = useSocketStore()
+</script>
+
+<style scoped>
+.message-list {
+  background: #222;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  padding: 5px 0;
+}
+</style>
